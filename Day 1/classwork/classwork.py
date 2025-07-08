c83e624d5bcf039c1125fdff59a1ab71de70d1ca
JobavaLuka <lukajobava0@gmail.com>
2025-07-08 18:25:34 +0400

    commit

---
+++ Day 1/classwork/classwork.py
@@ -0,0 +1,18 @@
+num1 = 30
+num2 = 10
+
+print(num1 + num2)
+
+
+num3 = 140
+num4 = 70
+
+plus = num3 + num4
+minus = num3 - num4
+multiply = num3 * num4
+divide = num3 / num4
+
+print(plus)
+print(minus)
+print(multiply)
+print(divide)
---
+++ Day 2/homework/homework.py
@@ -0,0 +1,80 @@
+#I დავალება:
+# ორი ცვლადი
+a = 10
+b = 5
+
+# დამატება
+print(a + b)  # 10 + 5 = 15
+
+# გამოკლება
+print(a - b)  # 10 - 5 = 5
+
+# გამრავლება
+print(a * b)  # 10 * 5 = 50
+
+# გაყოფა
+print(a / b)  # 10 / 5 = 2.0
+
+
+#II დავალება:
+# სტრინგ ტიპის ცვლადები
+name = "Luka"
+city = "Tbilisi"
+hobby = "wrestling"
+language = "Python"
+
+# მთელი რიცხვები (integer)
+age = 16
+year = 2009
+score = 95
+height_cm = 175
+
+# ათწილადი რიცხვები (float)
+weight = 59.40
+temperature = 36.6
+price = 19.99
+distance_km = 12.5
+
+# დაბეჭდვა ცვლადების გამოყენებით
+print(name)
+print(city)
+print(hobby)
+print(language)
+
+print(age)
+print(year)
+print(score)
+print(height_cm)
+
+print(weight)
+print(temperature)
+print(price)
+print(distance_km)
+
+
+#III დავალება:
+# სტრინგ ტიპის ორი ცვლადი
+first_name = "Luka"
+last_name = "Jobava"
+
+# სტრინგების დამატება
+full_name = first_name + last_name
+
+print(full_name)
+
+# ამ მოქმედებას ჰქვია "კონკატენაცია" ანუ სტრინგების შეერთება
+# ეს ხდება მაშინ, როცა ორი ან მეტი ტექსტური მონაცემი ერთმანეთს ემატება
+
+
+# 5 სტრინგ ტიპის ცვლადი
+name1 = "giorgi"
+name2 = "goga"
+name3 = "lasha"
+name4 = "beka"
+name5 = "nika"
+
+# ყველა ცვლადის შერთვა ერთ წინადადებად
+result = name1 + name2 + name3 + name4 + name5
+
+# შედეგის დაბეჭდვა
+print(result)
