b2a7ea72dd3f3355e3d1f759d6e65a4d0d301139
JobavaLuka <lukajobava0@gmail.com>
2025-07-09 00:39:28 +0400

    commit

--- Day 2/classwork/classwork.py
+++ Day 2/classwork/classwork.py
@@ -6,3 +6,59 @@ tv_show = "the sopranos"
 
 #ცვლადის დაბეჭდვა
 print(tv_show)
+
+
+#5 ცვლადი, ერთნაირი ასოებით, მაგრამ ყველა განსხვავებული
+name = "no big letters"
+Name = "first, big letter"
+nAme = "Second, big letter"
+naMe = "third, big letter"
+namE = "fourth, big letter"
+
+#დავპრინტოთ ყველა ცვლადი
+print(name)
+print(Name)
+print(nAme)
+print(naMe)
+print(namE)
+
+
+#String ტიპის ცვლადები
+first_name = "Luka"
+last_name = "Jobava"
+favorite_color = "Navy_blue"
+country_name = "Georgia"
+
+#Integer ტიპის ცვლადები
+age = 16
+rating = 6
+aura = 50
+birth_year = 2009
+
+#Float ტიპის ცვლადები
+height_meters = 1.75
+weight_kg = 59.40
+miles_ran = 13.5
+lenght = 18.7
+
+#დაბეჭდვა
+print(first_name)
+print(last_name)
+print(favorite_color)
+print(country_name)
+
+print(age)
+print(rating)
+print(aura)
+print(birth_year)
+
+print(height_meters)
+print(weight_kg)
+print(miles_ran)
+print(lenght)
+
+
+user_name = "goga"
+user_surname = "chalauri"
+first_number = 200
+second_number = 123
