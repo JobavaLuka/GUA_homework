b7518f22b8110d29fa54295418d807711d889947
JobavaLuka <lukajobava0@gmail.com>
2025-07-09 00:34:30 +0400

    commit

---
+++ Day 2/classwork/classwork.py
@@ -0,0 +1,8 @@
+#ცვლადის შექმნა და მნიშვნელობის მინიჭება
+tv_show = "breaking bad"
+
+#ცვლადის განახლება (update)
+tv_show = "the sopranos"
+
+#ცვლადის დაბეჭდვა
+print(tv_show)
--- Day 2/homework/homework.py
+++ Day 2/homework/homework.py
@@ -1,41 +1,32 @@
 #I დავალება:
-# ორი ცვლადი
 a = 10
 b = 5
 
-# დამატება
-print(a + b)  # 10 + 5 = 15
+print(a + b)
 
-# გამოკლება
-print(a - b)  # 10 - 5 = 5
+print(a - b)
 
-# გამრავლება
-print(a * b)  # 10 * 5 = 50
+print(a * b)
 
-# გაყოფა
-print(a / b)  # 10 / 5 = 2.0
+print(a / b)
 
 
 #II დავალება:
-# სტრინგ ტიპის ცვლადები
 name = "Luka"
 city = "Tbilisi"
 hobby = "wrestling"
 language = "Python"
 
-# მთელი რიცხვები (integer)
 age = 16
 year = 2009
 score = 95
 height_cm = 175
 
-# ათწილადი რიცხვები (float)
 weight = 59.40
 temperature = 36.6
 price = 19.99
 distance_km = 12.5
 
-# დაბეჭდვა ცვლადების გამოყენებით
 print(name)
 print(city)
 print(hobby)
@@ -53,11 +44,9 @@ print(distance_km)
 
 
 #III დავალება:
-# სტრინგ ტიპის ორი ცვლადი
 first_name = "Luka"
 last_name = "Jobava"
 
-# სტრინგების დამატება
 full_name = first_name + last_name
 
 print(full_name)
@@ -66,15 +55,29 @@ print(full_name)
 # ეს ხდება მაშინ, როცა ორი ან მეტი ტექსტური მონაცემი ერთმანეთს ემატება
 
 
-# 5 სტრინგ ტიპის ცვლადი
+#IV დავალება:
 name1 = "giorgi"
 name2 = "goga"
 name3 = "lasha"
 name4 = "beka"
 name5 = "nika"
 
-# ყველა ცვლადის შერთვა ერთ წინადადებად
 result = name1 + name2 + name3 + name4 + name5
 
-# შედეგის დაბეჭდვა
 print(result)
+
+
+#V დავალება:
+number1 = 5.5
+
+number2 = 4
+
+multiply = number1 * number2
+
+plus = number1 + number2
+
+minus = number1 - number2
+
+print(multiply)
+print(plus)
+print(minus)
