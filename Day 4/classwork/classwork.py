47d6abe66c33b9469f5d405295f9f4a9b786c4be
JobavaLuka <lukajobava0@gmail.com>
2025-07-18 17:42:50 +0400

    commit

---
+++ Day 4/classwork/classwork.py
@@ -0,0 +1,8 @@
+# მომხმარებელს შემოაქვს ორი რიცხვი
+num1 = int(input("Put in first number: "))
+num2 = int(input("Put in second number: "))
+
+# შედარებები
+print(num1 > num2)
+print(num1 < num2)
+print(num1 == num2)
