a023ec909478adfb13c0bcdaf0deeabf6d114412
JobavaLuka <lukajobava0@gmail.com>
2025-07-29 19:58:56 +0400

    commit

---
+++ Day 7/classwork/classwork.py
@@ -0,0 +1,13 @@
+#True
+print(5 > 3 and 2 < 4)
+print(7 == 2 or 10 > 5)
+print(4 == 4 and 5==5)
+
+#False
+print(6 < 3 and 2 == 2)
+print(8 == 8 and 5 > 10)
+print(3 > 5 or 4 == 1)
+
+
+for i in range(20):
+    print("Luka Jobava")
