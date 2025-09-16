238150f03aeeeb669ec0e9909699d2c67bbc72f5
JobavaLuka <lukajobava0@gmail.com>
2025-09-16 20:12:12 +0400

    commit

---
+++ Day 21/classwork/classwork.py
@@ -0,0 +1,23 @@
+box = ["luka", "nika", "goga", "gio", "giga", "gegi", "nuca", "lizi", "nikoli", "saba"]
+
+print(box[0])
+print(box[1])
+print(box[2])
+print(box[3])
+print(box[4])
+print(box[5])
+print(box[6])
+print(box[7])
+print(box[8])
+print(box[9])
+
+
+vegetables = ["კარტოფილი", "სტაფილო", "პომიდორი", "კიტრი", "ბადრიჯანი", "ხახვი"]
+
+vegetables[0] = "viri"
+
+vegetables[3] = "tagvi"
+
+vegetables[5] = "bokveri"
+
+print(vegetables)
