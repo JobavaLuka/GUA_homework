101df5e7b01ffe8a1c6671714e49ec2bac712b5b
JobavaLuka <lukajobava0@gmail.com>
2026-02-24 19:05:14 +0400

    commit

---
+++ Day 64/classwork/classwork.py
@@ -0,0 +1,17 @@
+def say_my_name():
+    print("my name is luka")
+
+say_my_name()
+say_my_name()
+say_my_name()
+say_my_name()
+
+
+def check_even():
+    numbers = 67
+    if numbers % 2 == 0:
+        return "this number is even"
+    else:
+        return "this number is odd"
+
+print(check_even())
